c3186fa6d09e6fd3c4fe6cc658fdec2187d2e430
emercy <emercyma@tencent.com>
2020-08-02 13:39:08 +0800

    【35】【截稿】页面视图：ViewPager

--- app/src/main/java/com/emercy/myapplication/MainActivity.java
+++ app/src/main/java/com/emercy/myapplication/MainActivity.java
@@ -3,78 +3,71 @@ package com.emercy.myapplication;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
+
+import androidx.viewpager.widget.ViewPager;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
-    private ViewFlipper mViewFlipper;
-    private Context mContext;
-    private float initialX;
+    private ViewPager mViewPager;
+    private String[] mTitle = new String[]{"苹果", "香蕉", "荔枝"};
+    private List<View> mView = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = this;
-        mViewFlipper = findViewById(R.id.view_flipper);
 
-        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+        final TextView tv = findViewById(R.id.text);
+        mViewPager = findViewById(R.id.view_pager);
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View view) {
-                mViewFlipper.setAutoStart(true);
-                mViewFlipper.setInAnimation(mContext, R.anim.in_from_right);
-                mViewFlipper.setOutAnimation(mContext, R.anim.out_from_left);
-                mViewFlipper.setFlipInterval(2000);
-                mViewFlipper.startFlipping();
-                Toast.makeText(MainActivity.this,
-                        "启动自动播放", Toast.LENGTH_SHORT).show();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tv.setText(mTitle[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
 
+        ImageView view1 = (ImageView) LayoutInflater.from(this).inflate(R.layout.list_item, null);
+        view1.setBackgroundColor(Color.RED);
+        view1.setImageResource(R.drawable.apple);
+        mView.add(view1);
 
-        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mViewFlipper.stopFlipping();
-                Toast.makeText(MainActivity.this,
-                        "停止自动播放", Toast.LENGTH_SHORT).show();
-            }
-        });
+        ImageView view2 = (ImageView) LayoutInflater.from(this).inflate(R.layout.list_item, null);
+        view2.setBackgroundColor(Color.GREEN);
+        view2.setImageResource(R.drawable.banana);
+        mView.add(view2);
 
-    }
+        ImageView view3 = (ImageView) LayoutInflater.from(this).inflate(R.layout.list_item, null);
+        view3.setBackgroundColor(Color.BLUE);
+        view3.setImageResource(R.drawable.lychee);
+        mView.add(view3);
 
-    @Override
-    public boolean onTouchEvent(MotionEvent touchEvent) {
-        switch (touchEvent.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                // 记录滑动初始坐标
-                initialX = touchEvent.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                // 记录滑动结束坐标
-                float finalX = touchEvent.getX();
-                if (initialX > finalX) {
-                    // 初始坐标大于结束坐标，说明为左滑，则播放下一页
-                    if (mViewFlipper.getDisplayedChild() != 2) {
-                        mViewFlipper.setInAnimation(mContext, R.anim.in_from_right);
-                        mViewFlipper.setOutAnimation(mContext, R.anim.out_from_left);
-                        mViewFlipper.showNext();
-                    }
-                } else {
-                    // 初始坐标不大于结束坐标，说明为右滑，则播放上一页
-                    if (mViewFlipper.getDisplayedChild() != 0) {
-                        mViewFlipper.setInAnimation(mContext, R.anim.in_from_left);
-                        mViewFlipper.setOutAnimation(mContext, R.anim.out_from_right);
-                        mViewFlipper.showPrevious();
-                    }
-                }
-                break;
-        }
-        return false;
+        mViewPager.setAdapter(new MyAdapter(mView));
+        tv.setText(mTitle[0]);
+
     }
 }
--- app/src/main/java/com/emercy/myapplication/MyAdapter.java
+++ app/src/main/java/com/emercy/myapplication/MyAdapter.java
@@ -1,55 +1,41 @@
-//package com.emercy.myapplication;
-//
-//import android.content.Context;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.BaseAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//
-//public class MyAdapter extends BaseAdapter {
-//
-//    private Context mContext;
-//    private String[] mName;
-//    private int[] mResId;
-//
-//    public MyAdapter(Context context) {
-//        mContext = context;
-//    }
-//
-//    public void setData(String[] name, int[] resId) {
-//        mName = name;
-//        mResId = resId;
-//    }
-//
-//
-//    @Override
-//    public int getCount() {
-//        return mName.length * 10;
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        return null;
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return position;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        // 针对convertView做一个简单的优化
-//        if (convertView == null) {
-//            convertView = LayoutInflater.from(mContext).inflate(R.layout.list_view, null);
-//        }
-//        TextView name = convertView.findViewById(R.id.textView);
-//        ImageView image = convertView.findViewById(R.id.imageView);
-//        name.setText(mName[position % mName.length]);
-//        image.setImageResource(mResId[position % mResId.length]);
-//        return convertView;
-//    }
-//}
+package com.emercy.myapplication;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class MyAdapter extends PagerAdapter {
+
+
+    private final List<View> mView;
+
+    public MyAdapter(List<View> view) {
+        mView = view;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        container.addView(mView.get(position));
+        return mView.get(position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView(mView.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mView.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+}
