40b83a985f0b303596a124197fb9acd28b2f2bb2
hust-MC <251887584@qq.com>
2020-11-17 21:00:00 +0800

    【截稿】39 Touch

--- app/src/main/java/com/emercy/myapplication/MainActivity.java
+++ app/src/main/java/com/emercy/myapplication/MainActivity.java
@@ -3,76 +3,63 @@ package com.emercy.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    float xAxis = 0f;
+    float yAxis = 0f;
 
-    private static final int MAX = 100;
-    private static final int START_PROGRESS = 100;
-    private static final int UPDATE_COUNT = 200;
+    float downXAxis = 0f;
+    float downYAxis = 0f;
 
-    private ProgressBar progressBar;
-    private Button startProgress;
-    private TextView textView;
-    private boolean mHasStart;
-
-    // 任务2：在主线程刷新进度条
-    Handler mHandlerThread = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            if (msg.what == START_PROGRESS) {
-                if (!mHasStart) {
-                    thread.start();
-                    mHasStart = true;
-                }
-            } else if (msg.what == UPDATE_COUNT) {
-                textView.setText("当前进度：" + msg.arg1 + "%");
-                progressBar.setProgress(msg.arg1);
-            }
-        }
-    };
-
-    // 任务1：在子线程执行耗时操作，通过sleep模拟耗时任务
-    Thread thread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            for (int i = 0; i <= 100; i++) {
-                try {
-                    // 一秒钟的耗时操作
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Message message = new Message();
-                message.what = UPDATE_COUNT;
-                message.arg1 = i;
-                mHandlerThread.sendMessage(message);
-            }
-        }
-    });
+    TextView downX, downY, moveX, moveY;
+    TextView touch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar = findViewById(R.id.progressBar);
-        startProgress = findViewById(R.id.start_progress);
-        textView = findViewById(R.id.textView);
-        progressBar.setMax(MAX);
 
-        startProgress.setOnClickListener(new View.OnClickListener() {
+        downX = findViewById(R.id.down_x);
+        downY = findViewById(R.id.down_y);
+        moveX = findViewById(R.id.move_x);
+        moveY = findViewById(R.id.move_y);
+
+        touch = findViewById(R.id.touch);
+
+        touch.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                // 任务3：通过Handler传递进度消息
-                Message message = new Message();
-                message.what = START_PROGRESS;
-                mHandlerThread.sendEmptyMessage(START_PROGRESS);
+            public boolean onTouch(View v, MotionEvent event) {
+                final int actionPeformed = event.getAction();
+
+                switch (actionPeformed) {
+                    case MotionEvent.ACTION_DOWN: {
+                        downXAxis = event.getX();
+                        downYAxis = event.getY();
+
+                        downX.setText("按下的位置横坐标：" + downXAxis);
+                        downY.setText("按下的位置纵坐标：" + downYAxis);
+                        break;
+                    }
+
+                    case MotionEvent.ACTION_MOVE: {
+                        final float x = event.getX();
+                        final float y = event.getY();
+
+                        final float dx = x - downXAxis;
+                        final float dy = y - downYAxis;
+
+                        xAxis += dx;
+                        yAxis += dy;
+
+                        moveX.setText("移动距离的横坐标：" + xAxis);
+                        moveY.setText("移动距离的纵坐标：" + yAxis);
+                        break;
+                    }
+                }
+                return true;
             }
         });
     }
