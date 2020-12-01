1a6832a05fd40f7f6abcad72cb4c9e8b079bfc6f
hust-MC <251887584@qq.com>
2020-12-01 21:23:43 +0800

    【截稿】45 file

--- app/src/main/java/com/emercy/myapplication/MainActivity.java
+++ app/src/main/java/com/emercy/myapplication/MainActivity.java
@@ -1,51 +1,71 @@
 
 package com.emercy.myapplication;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 
-    private static final String TAG = "ContentProvider";
+public class MainActivity extends Activity {
+
+    Button save, load;
+    TextView content;
+    EditText input;
+
+    String data;
+    private String file = "MC_data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.add).setOnClickListener(this);
-    }
+        save = findViewById(R.id.save);
+        load = findViewById(R.id.load);
 
-    @Override
-    public void onClick(View v) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-            int hasReadSmsPermission = checkSelfPermission(Manifest.permission.READ_CONTACTS);
-            if (hasReadSmsPermission != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 100);
-                return;
+        input = findViewById(R.id.editText);
+        content = findViewById(R.id.content);
+        save.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                data = input.getText().toString();
+                try {
+                    FileOutputStream fOut = openFileOutput(file, MODE_WORLD_READABLE);
+                    fOut.write(data.getBytes(StandardCharsets.UTF_8));
+                    fOut.close();
+                    Toast.makeText(getBaseContext(), "文件保存成功", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        }
+        });
 
-        ContentResolver resolver = getContentResolver();
-        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        Cursor cursor = resolver.query(uri, null, null, null, null);
-        while (cursor.moveToNext()) {
-            //获取联系人姓名,手机号码
-            String cName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String cNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            Log.d(TAG, "姓名:" + cName);
-            Log.d(TAG, "号码:" + cNum);
-        }
-        cursor.close();
+        load.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fin = openFileInput(file);
+                    byte[] buf = new byte[1024];
+                    int count;
+                    StringBuilder temp = new StringBuilder();
+                    while ((count = fin.read(buf)) > 0) {
+                        temp.append(new String(buf, 0, count, StandardCharsets.UTF_8));
+                    }
+                    content.setText(temp.toString());
+                    Toast.makeText(getBaseContext(), "文件加载成功", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
