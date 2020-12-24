6e145962f2ce682c058e2cefc7d2742810f19082
hust-MC <251887584@qq.com>
2020-12-24 16:27:37 +0800

    【截稿】 55 Bitmap

--- app/src/main/java/com/emercy/myapplication/MainActivity.java
+++ app/src/main/java/com/emercy/myapplication/MainActivity.java
@@ -3,20 +3,80 @@ package com.emercy.myapplication;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends Activity {
+    static ByteArrayOutputStream byteOut = null;
+    private Bitmap bitmap = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView button = (ImageView) findViewById(R.id.image);
-        InputStream resource = getResources().openRawResource(R.raw.avatar);
-        Bitmap bitmap = BitmapFactory.decodeStream(resource);
-        button.setBackground(new RoundCornerDrawable(bitmap));
+        Button btn_cut = (Button) findViewById(R.id.button);
+        btn_cut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
+    }
+
+    public void capture() {
+        Runnable action = new Runnable() {
+            @Override
+            public void run() {
+                final View contentView = getWindow().getDecorView();
+                try {
+                    bitmap = Bitmap.createBitmap(contentView.getWidth(),
+                            contentView.getHeight(), Bitmap.Config.ALPHA_8);
+                    contentView.draw(new Canvas(bitmap));
+                    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteOut);
+                    save(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (null != byteOut)
+                            byteOut.close();
+                        if (null != bitmap && !bitmap.isRecycled()) {
+                            bitmap = null;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        };
+        try {
+            action.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void save(Bitmap b) {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream("sdcard/short.png");
+            boolean success = b.compress(Bitmap.CompressFormat.PNG, 80, fos);
+            fos.flush();
+            fos.close();
+            if (success) {
+                Toast.makeText(MainActivity.this, "截图完成", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
